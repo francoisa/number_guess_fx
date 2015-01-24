7f3dc588aad007073d84982a7d8a230e70d4faec
francoisa <andre.francois@gmail.com>
2015-01-23 23:57:22 -0500

    This is the correct UI

--- src/number_guess_fx/FXMLDocumentController.java
+++ src/number_guess_fx/FXMLDocumentController.java
@@ -6,11 +6,13 @@
 package number_guess_fx;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
 
 /**
  *
@@ -19,17 +21,42 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label guessLbl;
+    
+    private int answer;
+    private int num;
+    private final int tries = 5;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
+        System.exit(0);
+    }
+    
+    @FXML
+    private void handleCheckboxAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        CheckBox cb = (CheckBox) event.getSource();
+        int guess = Integer.parseInt(cb.getText());
+        cb.setDisable(true);
+        if (guess != answer) {
+            guessLbl.setText(guess + " is not the number. " + ++num + " of " + tries);
+        }
+        else {
+            guessLbl.setText(guess + " is the number!");
+        }
+        if (num == tries) {
+            guessLbl.setText("No luck. The number was: " + answer);
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        number();
     }    
     
+    public void number() {
+        Random random = new Random(System.currentTimeMillis());
+	answer = Math.round(10*random.nextFloat()) + 1;
+        num = 0;
+    }
 }
