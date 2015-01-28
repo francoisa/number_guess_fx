a1e3cb256540150fbc8557dbb178115980ca07ec
francoisa <andre.francois@gmail.com>
2015-01-27 22:16:30 -0500

    Fixed the restart functionality.

--- src/number_guess_fx/FXMLDocumentController.java
+++ src/number_guess_fx/FXMLDocumentController.java
@@ -8,11 +8,16 @@ package number_guess_fx;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.Stack;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -22,14 +27,35 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label guessLbl;
+  
+    @FXML
+    private Button restartBtn;
+    
+    @FXML
+    private VBox cbContainer;
+    
+    private Stack<CheckBox> cbStack;
     
     private int answer;
     private int num;
     private final int tries = 5;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void handleQuitAction(ActionEvent event) {
         System.exit(0);
+    }
+    
+    @FXML
+    private void handleRestartAction(ActionEvent event) {
+        number();
+        guessLbl.setText("");
+        restartBtn.setDisable(true);
+        enableCheckboxes();
+        while (!cbStack.isEmpty()) {
+            CheckBox cb = cbStack.pop();
+            cb.setSelected(false);
+            cb.setDisable(false);
+        }
     }
     
     @FXML
@@ -38,19 +64,33 @@ public class FXMLDocumentController implements Initializable {
         CheckBox cb = (CheckBox) event.getSource();
         int guess = Integer.parseInt(cb.getText());
         cb.setDisable(true);
+        cbStack.push(cb);
         if (guess != answer) {
             guessLbl.setText(guess + " is not the number. " + ++num + " of " + tries);
         }
         else {
+            disableCheckboxes();
+            restartBtn.setDisable(false);
             guessLbl.setText(guess + " is the number!");
         }
         if (num == tries) {
+            disableCheckboxes();
+            restartBtn.setDisable(false);
             guessLbl.setText("No luck. The number was: " + answer);
         }
     }
     
+    private void disableCheckboxes() {
+        cbContainer.setDisable(true);
+    }
+
+    private void enableCheckboxes() {
+        cbContainer.setDisable(false);
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        cbStack = new Stack<>();
+        restartBtn.setDisable(true);
         number();
     }    
     
